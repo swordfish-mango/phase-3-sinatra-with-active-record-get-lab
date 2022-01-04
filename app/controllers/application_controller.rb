29283592ee971fcdf49c02165daebc8fc245f528
Marcus <marcus.shimotsu@gmail.com>
2022-01-04 10:25:35 -0600

    commit tree 87f19e23342dd9724a903f842852d571d69d4044

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, :json
 
-  # add routes
+  get '/bakeries' do
+    Bakery.all.to_json
+  end
+
+  get '/bakeries/:id' do
+    Bakery.find(params[:id]).to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    priceSorted.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    priceSorted.limit(1).to_json
+  end
+
+  def priceSorted
+    BakedGood.all.order(price: :desc)
+  end
 
 end
